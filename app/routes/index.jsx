51af3cc50d5c38ca78e1cbb7f1e3f883a5795f8c
RafaelAdao <rafael.adao@hotmail.com.br>
2023-02-01 22:03:54 -0300

    start some frontend UI

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,17 +1,55 @@
-import stylesUrl from '../styles/index.css'
-
-export let links = () => {
-  return [{ rel: 'stylesheet', href: stylesUrl }]
-}
+import { useState } from 'react'
 
 export default function Index() {
+  const [profileURL, setProfileURL] = useState('')
+  const [bookList, setBookList] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+
+    if (!profileURL.match(/https:\/\/www.skoob.com.br\/usuario\/\d+/)) {
+      alert('Link inválido')
+      return
+    }
+    setLoading(true)
+    const response = await fetch('http://localhost:3001/profile', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ profileURL })
+    })
+
+    const { books } = await response.json()
+    setBookList(books)
+    setLoading(false)
+  }
+
   return (
-    <section className="layout">
-      <div className="header">1</div>
-      <div className="leftSide">2</div>
-      <div className="body">3</div>
-      <div className="rightSide">4</div>
-      <div className="footer">5</div>
-    </section>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Link do perfil do Skoob
+          <input
+            type="text"
+            value={profileURL}
+            onChange={event => setProfileURL(event.target.value)}
+          />
+        </label>
+        <button type="submit">OK</button>
+      </form>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        bookList.length > 0 && (
+          <ul>
+            {bookList.map(book => (
+              <li key={book.id}>{book.title}</li>
+            ))}
+          </ul>
+        )
+      )}
+    </div>
   )
 }
